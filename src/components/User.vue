3df4678c9a22f6ec055788073b89f162d1acf4bd
Gustavo Ramírez <gustavo81988@gmail.com>
2018-04-29 19:31:22 -0500

    Communication between Sibling Components

--- src/components/User.vue
+++ src/components/User.vue
@@ -11,11 +11,16 @@
                 :myName="name"
                 @nameWasReset="name = $event"
                 :resetFn="resetName"
+                :userAge="age"
             >
             </app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit
+                    :userAge="age"
+                    @ageWasEdited="age = $event"
+                >
+                </app-user-edit>
             </div>
         </div>
     </div>
@@ -28,7 +33,8 @@
     export default {
         data: function(){
             return {
-                name: 'Gustavo'
+                name : 'Gustavo',
+                age  : 27
             }
         },
         methods: {
