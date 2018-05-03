8064a32d2c299110e0d7b317db7642194fd59be2
Gustavo Ramírez <gustavo81988@gmail.com>
2018-05-02 19:44:16 -0500

    Using Getters

--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,10 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state: {
         counter: 0
+    },
+    getters: {
+        dobuleCounter: state => {
+            return state.counter * 2
+        }
     }
 })
